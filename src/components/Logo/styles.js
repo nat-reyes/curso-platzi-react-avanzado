1ff8f19a51dc89cf802cd6dfdb2694fa7bd503d0
Nat Reyes <aylin.reyes.8@gmail.com>
2021-04-22 20:56:47 -0500

    Categories from the API and advances

---
+++ src/components/Logo/styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Svg = styled.svg`
+  object-fit: cover;
+  width: 100%;
+`
+
+export const Wrapper = styled.div`
+  width: 100%;
+  box-shadow: 0 10px 14px rgba(0, 0, 0, .05);
+  display: flex;
+  align-items: flex-start;
+  margin-bottom: 10px;
+`
